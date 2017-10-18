a70fdbca6dbc9553a78b8a6f7f9624401bd2c582
tayojays <tayojays@gmail.com>
2017-10-18 13:33:50 +0100

    heroku database config commit

--- config/database.php
+++ config/database.php
@@ -63,11 +63,11 @@ return [
 
         'herokumysql' => [
             'driver' => 'mysql',
-            'host' = $host,
-            'port' = 3306,
-            'database' = $database,
-            'username' = $username,
-            'password' = $password,
+            'host' => $host,
+            'port' => env('DB_PORT', '3306'),
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
